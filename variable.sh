0a83bfb56cfa4b46d08a1a7621f32962e449e061
pappik <pappik@gmail.com>
2023-01-06 16:23:42 -0500

    variable and if testing

--- variable.sh
+++ variable.sh
@@ -1,7 +1,7 @@
 name="rao"
 echo "$name"
 
-if [[ "$name" == "rao" ]];
+if [ "$name" = "rao" ];
 then
   echo "welcome paparao"
 else
@@ -10,7 +10,7 @@ fi
 
 sis=$1
 
-if [[ "$sis" -eq "prema" ]];
+if [ "$sis" = "prema" ];
 then
   echo "sissy"
 else
