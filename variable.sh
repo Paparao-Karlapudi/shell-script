d64a63fa579b045bddc951d4280d3c82f7021f33
pappik <pappik@gmail.com>
2023-01-06 15:52:36 -0500

    variable and if testing

---
+++ variable.sh
@@ -0,0 +1,18 @@
+name=rao
+echo "$name"
+
+if [ $name == rao]
+then
+  echo "welcome paparao"
+else
+  echo "unauth entry"
+
+fi
+sis=$1
+
+if [ $sis == prema]
+then
+  echo "sissy"
+else
+  echo "sorry not sissy"
+fi
