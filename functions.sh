734796a56d0f982c088a10578d38cef53e87ffff
pappik <pappik@gmail.com>
2023-08-27 08:50:37 -0400

    added success message

---
+++ functions.sh
@@ -0,0 +1,11 @@
+#functions are like variables
+#i.e when we assign a name to a set of commands then its called a function.
+
+
+#function declaration
+example() {
+  echo i am a function
+}
+
+#calling a function
+example
