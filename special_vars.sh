6b020394ab94dbf3b0da6900b5f5b38b5ec27a10
pappik <pappik@gmail.com>
2023-08-28 15:50:00 -0400

    functions with args

---
+++ special_vars.sh
@@ -0,0 +1,22 @@
+echo "following values from script"
+echo "0 -$0" #it gives the filename as output
+echo "1 -$1" #it gives the first passed argument as o/p
+echo "2 -$2" # it gives 2nd passes argument as o/p
+echo "* -$*" #it gives all the given arguments as o/p
+echo "@ -$@"
+echo "# -$#" #it gives the number of given arguments as o/p
+
+echo "following values from function"
+example(){
+  echo "0 -$0"
+  echo "1 -$1" #it gives the first passed argument as o/p
+  echo "2 -$2" # it gives 2nd passes argument as o/p
+  echo "* -$*" #it gives all the given arguments as o/p
+  echo "@ -$@" #it is practically same as $* but differs theoretically
+  echo "# -$#" #it gives the number of given arguments as o/p
+
+}
+
+example
+
+#Note: here the function do
